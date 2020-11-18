eeaeb5e40f14c5d6eeb75beabb1b5bc0531df424
kotosanagi <kotosanagi@gmail.com>
2020-11-18 21:50:44 +0900

    write codes

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,11 +6,26 @@
 /*   By: skotoyor <skotoyor@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/15 21:13:23 by skotoyor          #+#    #+#             */
-/*   Updated: 2020/11/15 21:15:21 by skotoyor         ###   ########.fr       */
+/*   Updated: 2020/11/18 11:50:42 by skotoyor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# define MAX_FD	 30
+
+int	get_next_line(int fd, char **line);
+int	read_from_fd(int fd, char **line, char **st_arr);
+int	find_newline(int fd, char **line, char **st_arr, char *buf);
+
+size_t	ft_strchr_len(char *s, char c);
+size_t	ft_strlen(char *s);//この関数なくてもok
+char	*ft_strnjoin(char *s1, char *s2, size_t len_s2);// len_s2 is like n;
+char	*ft_strdup(char *s1);
+int	free_all(char **line, char **st_arr, int fd);
 
 #endif
--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -6,7 +6,88 @@
 /*   By: skotoyor <skotoyor@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/15 21:16:31 by skotoyor          #+#    #+#             */
-/*   Updated: 2020/11/15 21:16:54 by skotoyor         ###   ########.fr       */
+/*   Updated: 2020/11/18 11:48:48 by skotoyor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+size_t	ft_strchr_len(char *s, char c)
+{
+	size_t i;
+
+	i = 0;
+	while (s[i] && s[i] != c)
+		i++;
+	return (i);
+}
+
+size_t	ft_strlen(char *s)//この関数なくてもok
+{
+	size_t i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strnjoin(char *s1, char *s2, size_t len_s2)// len_s2 is like n
+{
+	char *ret;
+	size_t len_s1;
+	size_t i;
+	size_t s_count;
+
+	if (!s1 || !s2)
+		return (NULL);
+	len_s1 = ft_strlen(s1);
+	if (!(ret = (char *)malloc(len_s1 + len_s2 + 1)))
+		return (NULL);
+	i = 0;
+	s_count = 0;
+	while (s1[s_count])
+		ret[i++] = s1[s_count++];
+	s_count = 0;
+	while (s_count < len_s2)
+		ret[i++] = s2[s_count++];
+	ret[i] = '\0';
+	return (ret);
+}
+
+
+char	*ft_strdup(char *s1)
+{
+	char	*arr;
+	size_t	len;
+	size_t	i;
+
+	len = ft_strlen(s1);
+	arr = (char *)malloc(sizeof(char) * (len + 1));
+	if (arr == NULL)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		arr[i] = s1[i];
+		i++;
+	}
+	arr[i] = '\0';
+	return (arr);
+}
+
+int	free_all(char **line, char **st_arr, int fd)
+{
+	free(*line);
+	free(st_arr[fd]);
+	return (-1);
+}
+
+// #include <stdio.h>
+// int main(){
+// 	char s1[] = "kotomocoooooo";
+
+
+// 	printf("%s\n", ft_strdup(s1));
+
+// }
